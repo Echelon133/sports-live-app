6a27e863e52b46174d0d489f868a60589acaa1a9
Mateusz <echelon133@gmail.com>
2023-08-16 15:32:16 +0200

    Implement tests of VenueService's `createVenue`

--- match-service/src/test/java/ml/echelon133/matchservice/venue/service/VenueServiceTests.java
+++ match-service/src/test/java/ml/echelon133/matchservice/venue/service/VenueServiceTests.java
@@ -2,11 +2,13 @@ package ml.echelon133.matchservice.venue.service;
 
 import ml.echelon133.common.exception.ResourceNotFoundException;
 import ml.echelon133.common.venue.dto.VenueDto;
+import ml.echelon133.matchservice.venue.model.UpsertVenueDto;
 import ml.echelon133.matchservice.venue.model.Venue;
 import ml.echelon133.matchservice.venue.repository.VenueRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,7 +18,11 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class VenueServiceTests {
@@ -60,5 +66,28 @@ public class VenueServiceTests {
         assertEquals(testEntity.getId(), dto.getId());
         assertEquals(testEntity.getName(), dto.getName());
         assertEquals(testEntity.getCapacity(), dto.getCapacity());
+    }
+
+    @Test
+    @DisplayName("createVenue calls repository's save and returns correct dto")
+    public void createVenue_ValidDto_CorrectlySavesAndReturns() {
+        var idToSave = UUID.randomUUID();
+        var initialDto = new UpsertVenueDto("Alianz Arena", 75024);
+        var entity = new Venue(initialDto.getName(), initialDto.getCapacity());
+        entity.setId(idToSave);
+
+        // given
+        given(venueRepository.save(argThat(v ->
+                v.getName().equals(initialDto.getName()) && v.getCapacity().equals(initialDto.getCapacity())
+        ))).willReturn(entity);
+
+        // when
+        VenueDto savedDto = venueService.createVenue(initialDto);
+
+        // then
+        verify(venueRepository, times(1)).save(any());
+        assertEquals(entity.getId(), savedDto.getId());
+        assertEquals(entity.getName(), savedDto.getName());
+        assertEquals(entity.getCapacity(), savedDto.getCapacity());
     }
 }
