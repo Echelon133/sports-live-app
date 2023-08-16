95e1bfd0675c1f5196db60a8c6086121634b8ad7
Mateusz <echelon133@gmail.com>
2023-08-16 16:36:43 +0200

    Implement VenueController's GET /api/venues/:id

--- match-service/src/main/java/ml/echelon133/matchservice/venue/controller/VenueController.java
+++ match-service/src/main/java/ml/echelon133/matchservice/venue/controller/VenueController.java
@@ -2,11 +2,8 @@ package ml.echelon133.matchservice.venue.controller;
 
 import ml.echelon133.common.exception.FormInvalidException;
 import ml.echelon133.common.exception.ResourceNotFoundException;
-import ml.echelon133.common.exception.ValidationResultMapper;
 import ml.echelon133.common.venue.dto.VenueDto;
 import ml.echelon133.matchservice.venue.model.UpsertVenueDto;
-import ml.echelon133.matchservice.venue.model.Venue;
-import ml.echelon133.matchservice.venue.repository.VenueRepository;
 import ml.echelon133.matchservice.venue.service.VenueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -19,7 +16,7 @@ import java.util.UUID;
 @RequestMapping("/api/venues")
 public class VenueController {
 
-    private VenueService venueService;
+    private final VenueService venueService;
 
     @Autowired
     public VenueController(VenueService venueService) {
@@ -28,7 +25,7 @@ public class VenueController {
 
     @GetMapping("/{id}")
     public VenueDto getVenue(@PathVariable UUID id) throws ResourceNotFoundException {
-        return null;
+        return venueService.findById(id);
     }
 
     @GetMapping
