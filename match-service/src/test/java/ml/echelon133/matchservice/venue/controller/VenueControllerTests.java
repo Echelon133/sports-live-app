4f82e2aa199cd5e02b034d79e8ab7240aaed0bef
Mateusz <echelon133@gmail.com>
2023-08-17 17:02:54 +0200

    Implement tests of VenueController's POST /api/venues

--- match-service/src/test/java/ml/echelon133/matchservice/venue/controller/VenueControllerTests.java
+++ match-service/src/test/java/ml/echelon133/matchservice/venue/controller/VenueControllerTests.java
@@ -3,6 +3,7 @@ package ml.echelon133.matchservice.venue.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ml.echelon133.common.exception.ResourceNotFoundException;
 import ml.echelon133.common.venue.dto.VenueDto;
+import ml.echelon133.matchservice.venue.model.UpsertVenueDto;
 import ml.echelon133.matchservice.venue.model.Venue;
 import ml.echelon133.matchservice.venue.service.VenueService;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,11 +18,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -41,6 +44,8 @@ public class VenueControllerTests {
     private VenueController venueController;
 
     private JacksonTester<VenueDto> jsonVenueDto;
+
+    private JacksonTester<UpsertVenueDto> jsonUpsertVenueDto;
 
     @BeforeEach
     public void beforeEach() {
@@ -87,5 +92,107 @@ public class VenueControllerTests {
         )
                 .andExpect(status().isOk())
                 .andExpect(content().string(expectedJson));
+    }
+
+    @Test
+    @DisplayName("POST /api/venues returns 422 when name is not provided")
+    public void insertVenue_NameNotProvided_StatusUnprocessableEntity() throws Exception {
+        var contentDto = new UpsertVenueDto(null, 100);
+        var json = jsonUpsertVenueDto.write(contentDto).getJson();
+
+        mvc.perform(
+                post("/api/venues")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(json)
+        )
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(
+                        jsonPath("$.messages", hasEntry("name", List.of("field has to be provided")))
+                );
+    }
+
+    @Test
+    @DisplayName("POST /api/venues returns 422 when name length is incorrect")
+    public void insertVenue_NameLengthIncorrect_StatusUnprocessableEntity() throws Exception {
+        var incorrectNameLengths = List.of(
+                // too short (0 characters)
+                "",
+                /// too long (121 characters)
+                "6EUxnEDmbw2qPIeUhhjnVrLJbnjdslg8dp1HUac4oK7tzkIo9QkQ125QTTT6oPmzk7pfo28BrGINDzbXBYZdKGTMhSEtOGf1g6wgbmJccybyeR3qFqpsEq2eC"
+        );
+
+        for (String incorrectName : incorrectNameLengths) {
+            var contentDto = new UpsertVenueDto(incorrectName, 100);
+            var json = jsonUpsertVenueDto.write(contentDto).getJson();
+
+            mvc.perform(
+                            post("/api/venues")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON)
+                                    .content(json)
+                    )
+                    .andExpect(status().isUnprocessableEntity())
+                    .andExpect(
+                            jsonPath("$.messages", hasEntry("name", List.of("expected length between 1 and 120")))
+                    );
+        }
+    }
+
+    @Test
+    @DisplayName("POST /api/venues returns 422 when capacity is negative")
+    public void insertVenue_CapacityNotPositive_StatusUnprocessableEntity() throws Exception {
+        var incorrectCapacities = List.of(-100, -1);
+
+        for (Integer incorrectCapacity: incorrectCapacities) {
+            var contentDto = new UpsertVenueDto("a", incorrectCapacity);
+            var json = jsonUpsertVenueDto.write(contentDto).getJson();
+
+            mvc.perform(
+                            post("/api/venues")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON)
+                                    .content(json)
+                    )
+                    .andExpect(status().isUnprocessableEntity())
+                    .andExpect(
+                            jsonPath("$.messages", hasEntry("capacity", List.of("expected positive integers")))
+                    );
+        }
+    }
+
+    @Test
+    @DisplayName("POST /api/venues returns 200 when name length is correct and capacity is positive")
+    public void insertVenue_NameLengthIsCorrectAndCapacityPositive_StatusOk() throws Exception {
+        var correctNameLengths = List.of(
+                // 1 character (lower limit)
+                "a",
+                // 120 characters (upper limit)
+                "6EUxnEDmbw2qPIeUhhjnVrLJbnjdslg8dp1HUac4oK7tzkIo9QkQ125QTTT6oPmzk7pfo28BrGINDzbXBYZdKGTMhSEtOGf1g6wgbmJccybyeR3qFqpsEq2e"
+        );
+
+        for (String correctName : correctNameLengths) {
+            // expected from the database
+            var dto = new VenueDto(UUID.randomUUID(), correctName, 0);
+            var expectedJson = jsonVenueDto.write(dto).getJson();
+
+            // what is given in the request body
+            var contentDto = new UpsertVenueDto(correctName, 0);
+            var bodyJson = jsonUpsertVenueDto.write(contentDto).getJson();
+
+            // use doReturn, because regular given/when does not work when re-declaring a single argThat matcher
+            doReturn(dto).when(venueService).createVenue(
+                    argThat(v -> v.getName().equals(contentDto.getName()) && v.getCapacity().intValue() == contentDto.getCapacity().intValue())
+            );
+
+            mvc.perform(
+                            post("/api/venues")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON)
+                                    .content(bodyJson)
+                    )
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(expectedJson));
+        }
     }
 }
