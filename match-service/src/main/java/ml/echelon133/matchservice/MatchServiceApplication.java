cba8ff661ae35153e84986a1f098dd1f3e2da664
Mateusz <echelon133@gmail.com>
2023-08-25 14:58:21 +0200

    Enable JPA auditing to automatically set `dateCreated` of entities

--- match-service/src/main/java/ml/echelon133/matchservice/MatchServiceApplication.java
+++ match-service/src/main/java/ml/echelon133/matchservice/MatchServiceApplication.java
@@ -3,9 +3,11 @@ package ml.echelon133.matchservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class MatchServiceApplication {
 
 	public static void main(String[] args) {
