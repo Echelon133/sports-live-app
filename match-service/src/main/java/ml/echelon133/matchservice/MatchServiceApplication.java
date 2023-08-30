6870af2601666200820f2e53d2919be609ed8e96
Mateusz <echelon133@gmail.com>
2023-08-30 11:27:25 +0200

    Configure a bean returning an ObjectMapper that knows how to work with LocalDate objects

--- match-service/src/main/java/ml/echelon133/matchservice/MatchServiceApplication.java
+++ match-service/src/main/java/ml/echelon133/matchservice/MatchServiceApplication.java
@@ -1,8 +1,11 @@
 package ml.echelon133.matchservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -14,4 +17,8 @@ public class MatchServiceApplication {
 		SpringApplication.run(MatchServiceApplication.class, args);
 	}
 
+	@Bean
+	public static ObjectMapper objectMapper() {
+		return new ObjectMapper().registerModules(new JavaTimeModule());
+	}
 }
