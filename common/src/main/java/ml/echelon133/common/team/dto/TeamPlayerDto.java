bb0b8fe8d98fd63fd5ac60b10079e7fbd9138079
Mateusz <echelon133@gmail.com>
2023-09-08 13:38:02 +0200

    Add TeamPlayer's dto and entity classes

---
+++ common/src/main/java/ml/echelon133/common/team/dto/TeamPlayerDto.java
@@ -0,0 +1,73 @@
+package ml.echelon133.common.team.dto;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public interface TeamPlayerDto {
+    UUID getId();
+    String getPosition();
+    Integer getNumber();
+
+    // if country is deleted, set this value to null to prevent any leakage of data (seems to be the simplest solution while using native queries)
+    @Value("#{target.countryDeleted ? null : (target.countryCode)}")
+    String getCountryCode();
+
+    @Value("#{T(ml.echelon133.common.team.dto.TeamPlayerDto.PlayerShortInfoDto).from(target.playerId, target.name, target.dateOfBirth)}")
+    PlayerShortInfoDto getPlayer();
+
+    static TeamPlayerDto from(UUID id, PlayerShortInfoDto playerShortInfoDto, String position, Integer number, String countryCode) {
+        return new TeamPlayerDto() {
+            @Override
+            public UUID getId() {
+                return id;
+            }
+
+            @Override
+            public PlayerShortInfoDto getPlayer() {
+                return playerShortInfoDto;
+            }
+
+            @Override
+            public String getPosition() {
+                return position;
+            }
+
+            @Override
+            public Integer getNumber() {
+                return number;
+            }
+
+            @Override
+            public String getCountryCode() {
+                return countryCode;
+            }
+        };
+    }
+
+    interface PlayerShortInfoDto {
+        UUID getId();
+        String getName();
+        LocalDate getDateOfBirth();
+
+        static PlayerShortInfoDto from(UUID playerId, String name, LocalDate dateOfBirth) {
+            return new PlayerShortInfoDto() {
+                @Override
+                public UUID getId() {
+                    return playerId;
+                }
+
+                @Override
+                public String getName() {
+                    return name;
+                }
+
+                @Override
+                public LocalDate getDateOfBirth() {
+                    return dateOfBirth;
+                }
+            };
+        }
+    }
+}
---
+++ match-service/src/main/java/ml/echelon133/matchservice/team/model/TeamPlayer.java
@@ -0,0 +1,73 @@
+package ml.echelon133.matchservice.team.model;
+
+import ml.echelon133.common.entity.BaseEntity;
+import ml.echelon133.matchservice.player.model.Player;
+import ml.echelon133.matchservice.player.model.Position;
+
+import javax.persistence.*;
+
+@Entity
+@Table(
+        name = "team_player",
+        indexes = {
+                @Index(columnList = "team_id", name = "team_id_index"),
+                @Index(columnList = "player_id", name = "player_id_index"),
+        }
+)
+public class TeamPlayer extends BaseEntity {
+
+    @ManyToOne(optional = false, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "team_id", nullable = false)
+    private Team team;
+
+    @ManyToOne(optional = false, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "player_id", nullable = false)
+    private Player player;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Position position;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    public TeamPlayer() {}
+    public TeamPlayer(Team team, Player player, Position position, Integer number) {
+        this.team = team;
+        this.player = player;
+        this.position = position;
+        this.number = number;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
