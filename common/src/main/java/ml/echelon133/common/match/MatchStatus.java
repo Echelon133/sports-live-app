07c8715e0d628fe8b604d432bc3ef9a465d742e3
Mateusz <echelon133@gmail.com>
2023-09-19 10:40:30 +0200

    Add MatchStatus enum with all possible statuses that a match can have

---
+++ common/src/main/java/ml/echelon133/common/match/MatchStatus.java
@@ -0,0 +1,15 @@
+package ml.echelon133.common.match;
+
+/**
+ * All possible statuses of a match.
+ */
+public enum MatchStatus {
+    NOT_STARTED,
+    FIRST_HALF,
+    HALF_TIME,
+    SECOND_HALF,
+    FINISHED,
+    EXTRA_TIME,
+    PENALTIES,
+    POSTPONED
+}
