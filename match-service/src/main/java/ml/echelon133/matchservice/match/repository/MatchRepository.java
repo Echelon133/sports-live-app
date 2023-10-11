ff90d88e73a99535a8b6b0a16e38f192469eb1c6
Mateusz <echelon133@gmail.com>
2023-10-11 16:23:12 +0200

    Implement MatchRepository's findAllBetween (with tests)

--- match-service/src/main/java/ml/echelon133/matchservice/match/repository/MatchRepository.java
+++ match-service/src/main/java/ml/echelon133/matchservice/match/repository/MatchRepository.java
@@ -1,12 +1,16 @@
 package ml.echelon133.matchservice.match.repository;
 
+import ml.echelon133.common.match.dto.CompactMatchDto;
 import ml.echelon133.common.match.dto.MatchDto;
 import ml.echelon133.common.match.dto.MatchStatusDto;
 import ml.echelon133.matchservice.match.model.Match;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -64,4 +68,32 @@ public interface MatchRepository extends JpaRepository<Match, UUID> {
     @Modifying
     @Query(value = "UPDATE match SET deleted = true WHERE id = :matchId AND deleted = false", nativeQuery = true)
     Integer markMatchAsDeleted(UUID matchId);
+
+    /**
+     * Finds all matches that start between the two dates specified in the arguments.
+     *
+     * @param startUTC start of the search period (in UTC)
+     * @param endUTC end of the search period (in UTC)
+     * @param pageable information about the wanted page
+     * @return a list of matches that start between the two dates
+     */
+    // CAST(id as varchar) is a workaround for https://github.com/spring-projects/spring-data-jpa/issues/1796
+    @Query(
+            value = "SELECT CAST(m.id as varchar) as id, m.status as status, m.result as result, " +
+                    "   m.start_time_utc as startTimeUTC, CAST(m.competition_id as varchar) as competitionId, " +
+                    "   m.half_time_home_goals as halfTimeHomeGoals, m.half_time_away_goals as halfTimeAwayGoals, " +
+                    "   m.home_goals as homeGoals, m.away_goals as awayGoals, " +
+                    "   m.home_penalties as homePenalties, m.away_penalties as awayPenalties, " +
+                    "CAST(ht.id as varchar) as homeTeamId, ht.name as homeTeamName, " +
+                    "   ht.crest_url as homeTeamCrestUrl, ht.deleted as homeTeamDeleted, " +
+                    "CAST(at.id as varchar) as awayTeamId, at.name as awayTeamName, " +
+                    "   at.crest_url as awayTeamCrestUrl, at.deleted as awayTeamDeleted " +
+                    "FROM match m " +
+                    "JOIN team ht ON m.home_team_id = ht.id " +
+                    "JOIN team at ON m.away_team_id = at.id " +
+                    "WHERE m.deleted = false AND m.start_time_utc BETWEEN :startUTC AND :endUTC " +
+                    "ORDER BY m.start_time_utc ASC",
+            nativeQuery = true
+    )
+    List<CompactMatchDto> findAllBetween(LocalDateTime startUTC, LocalDateTime endUTC, Pageable pageable);
 }
--- match-service/src/test/java/ml/echelon133/matchservice/match/repository/MatchRepositoryTests.java
+++ match-service/src/test/java/ml/echelon133/matchservice/match/repository/MatchRepositoryTests.java
@@ -2,6 +2,7 @@ package ml.echelon133.matchservice.match.repository;
 
 import ml.echelon133.common.match.MatchResult;
 import ml.echelon133.common.match.MatchStatus;
+import ml.echelon133.common.match.dto.CompactMatchDto;
 import ml.echelon133.common.match.dto.MatchDto;
 import ml.echelon133.matchservice.match.TestMatch;
 import ml.echelon133.matchservice.match.model.Match;
@@ -13,9 +14,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -238,6 +242,169 @@ public class MatchRepositoryTests {
         assertEquals(0, countDeleted);
     }
 
+    @Test
+    @DisplayName("findAllBetween native query does not fetch matches marked as deleted")
+    public void findAllBetween_MatchMarkedAsDeleted_SizeIsZero() {
+        // start the match 2023/01/01 8:00AM
+        var startDate = LocalDateTime.of(2023, 1, 1, 8, 0);
+        var matchToDelete = TestMatch.builder()
+                .startTimeUTC(startDate)
+                .deleted(true)
+                .build();
+        matchRepository.save(matchToDelete);
+
+        // when
+        var result = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 23, 59),
+                Pageable.unpaged()
+        );
+
+        // then
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    @DisplayName("findAllBetween native query does not leak deleted home team of a found match")
+    public void findAllBetween_HomeTeamDeleted_DoesNotLeakDeletedEntity() {
+        var teamBuilder = TestTeam.builder().deleted(true);
+        // start the match 2023/01/01 8:00AM
+        var startDate = LocalDateTime.of(2023, 1, 1, 8, 0);
+        var match = TestMatch.builder().startTimeUTC(startDate).homeTeam(teamBuilder).build();
+        matchRepository.save(match);
+
+        // when
+        var result = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 23, 59),
+                Pageable.unpaged()
+        );
+
+        // then
+        assertEquals(1, result.size());
+        var compactMatchDtoValue = result.get(0);
+        assertEntityAndDtoEqual(match, compactMatchDtoValue);
+        // make sure that the home team is definitely null
+        assertNull(compactMatchDtoValue.getHomeTeam());
+    }
+
+    @Test
+    @DisplayName("findAllBetween native query does not leak deleted away team of a found match")
+    public void findAllBetween_AwayTeamDeleted_DoesNotLeakDeletedEntity() {
+        var teamBuilder = TestTeam.builder().deleted(true);
+        // start the match 2023/01/01 8:00AM
+        var startDate = LocalDateTime.of(2023, 1, 1, 8, 0);
+        var match = TestMatch.builder().startTimeUTC(startDate).awayTeam(teamBuilder).build();
+        matchRepository.save(match);
+
+        // when
+        var result = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 23, 59),
+                Pageable.unpaged()
+        );
+
+        // then
+        assertEquals(1, result.size());
+        var compactMatchDtoValue = result.get(0);
+        assertEntityAndDtoEqual(match, compactMatchDtoValue);
+        // make sure that the away team is definitely null
+        assertNull(compactMatchDtoValue.getAwayTeam());
+    }
+
+    @Test
+    @DisplayName("findAllBetween native query only finds matches that start between two specified dates")
+    public void findAllBetween_MultipleMatches_OnlyFindsMatchesBetweenTwoDates() {
+        // create four matches:
+        //  0 -> starts 2023/01/01 00:00
+        var startTime0 = LocalDateTime.of(2023, 1, 1, 0, 0);
+        //  1 -> starts 2023/01/01 11:59PM
+        var startTime1 = LocalDateTime.of(2023, 1, 1, 23, 59);
+        //  2 -> starts 2023/01/02 00:00
+        var startTime2 = LocalDateTime.of(2023, 1, 2, 0, 0);
+        //  3 -> starts 2023/01/02 11:59PM
+        var startTime3 = LocalDateTime.of(2023, 1, 2, 23, 59);
+
+        var match0 = matchRepository.save(TestMatch.builder().startTimeUTC(startTime0).build());
+        var match1 = matchRepository.save(TestMatch.builder().startTimeUTC(startTime1).build());
+        var match2 = matchRepository.save(TestMatch.builder().startTimeUTC(startTime2).build());
+        var match3 = matchRepository.save(TestMatch.builder().startTimeUTC(startTime3).build());
+
+        // when
+        // find matches that started on 2023/01/01 (should return match0 and match1)
+        var result0 = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 23, 59),
+                Pageable.unpaged()
+        );
+        // find matches that started on 2023/01/02 (should return match2 and match3)
+        var result1 = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 2, 0, 0),
+                LocalDateTime.of(2023, 1, 2, 23, 59),
+                Pageable.unpaged()
+        );
+        // find matches between 2023/01/01 and 2023/01/04 (should return all matches)
+        var result2 = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 4, 0, 0),
+                Pageable.unpaged()
+        );
+
+        // then
+        // result0 should only contain match0 and match1
+        assertEquals(
+                2,
+                result0.stream().filter(m ->
+                        m.getId().equals(match0.getId()) || m.getId().equals(match1.getId())
+                ).count()
+        );
+        // result1 should only contain match2 and match3
+        assertEquals(
+                2,
+                result1.stream().filter(m ->
+                        m.getId().equals(match2.getId()) || m.getId().equals(match3.getId())
+                ).count()
+        );
+        // result2 should contain all four matches
+        assertEquals(4, result2.size());
+    }
+
+    @Test
+    @DisplayName("findAllBetween native query takes pageable into account")
+    public void findAllBetween_CustomPageable_ResultsPaged() {
+        var startDate = LocalDateTime.of(2023, 1, 1, 0, 0);
+        // create four random test matches
+        IntStream.range(0, 4)
+                .mapToObj(i -> TestMatch.builder().startTimeUTC(startDate).build())
+                .forEach(matchRepository::save);
+
+        // when
+        var pageable = Pageable.ofSize(1);
+        // first page should return 1 match
+        var result0 = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 23, 59),
+                pageable
+        );
+        // fourth page should return 1 match
+        var result1 = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 23, 59),
+                pageable.withPage(3)
+        );
+        // fifth page should be empty
+        var result2 = matchRepository.findAllBetween(
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 23, 59),
+                pageable.withPage(4)
+        );
+
+        // then
+        assertEquals(1, result0.size());
+        assertEquals(1, result1.size());
+        assertEquals(0, result2.size());
+    }
+
     private static void assertEntityAndDtoEqual(Match entity, MatchDto dto) {
         // simple fields equal
         assertTrue(
@@ -322,6 +489,70 @@ public class MatchRepositoryTests {
         assertTrue(
                 penaltiesEntity.getHomePenalties().equals(penaltiesDto.getHomePenalties()) &&
                 penaltiesEntity.getAwayPenalties().equals(penaltiesDto.getAwayPenalties())
+        );
+
+        // results equal
+        var resultDto = (dto.getResult() == null) ? null : MatchResult.valueOf(dto.getResult());
+        assertEquals(entity.getResult(), resultDto);
+    }
+
+    private static void assertEntityAndDtoEqual(Match entity, CompactMatchDto dto) {
+        // simple fields equal
+        assertTrue(
+                entity.getId().equals(dto.getId()) &&
+                        entity.getStatus().toString().equals(dto.getStatus()) &&
+                        entity.getCompetitionId().equals(dto.getCompetitionId()) &&
+                        entity.getStartTimeUTC().equals(dto.getStartTimeUTC())
+        );
+
+        // home teams equal
+        var homeTeamEntity = entity.getHomeTeam();
+        var shortHomeTeamDto = dto.getHomeTeam();
+        if (homeTeamEntity.isDeleted()) {
+            assertNull(shortHomeTeamDto);
+        } else {
+            assertTrue(
+                    homeTeamEntity.getId().equals(shortHomeTeamDto.getId()) &&
+                            homeTeamEntity.getName().equals(shortHomeTeamDto.getName()) &&
+                            homeTeamEntity.getCrestUrl().equals(shortHomeTeamDto.getCrestUrl())
+            );
+        }
+
+        // away teams equal
+        var awayTeamEntity = entity.getAwayTeam();
+        var shortAwayTeamDto = dto.getAwayTeam();
+        if (awayTeamEntity.isDeleted()) {
+            assertNull(shortAwayTeamDto);
+        } else {
+            assertTrue(
+                    awayTeamEntity.getId().equals(shortAwayTeamDto.getId()) &&
+                            awayTeamEntity.getName().equals(shortAwayTeamDto.getName()) &&
+                            awayTeamEntity.getCrestUrl().equals(shortAwayTeamDto.getCrestUrl())
+            );
+        }
+
+        // half time scores equal
+        var halfTimeScoreEntity = entity.getHalfTimeScoreInfo();
+        var halfTimeScoreDto = dto.getHalfTimeScoreInfo();
+        assertTrue(
+                halfTimeScoreEntity.getHomeGoals().equals(halfTimeScoreDto.getHomeGoals()) &&
+                        halfTimeScoreDto.getAwayGoals().equals(halfTimeScoreDto.getAwayGoals())
+        );
+
+        // scores equal
+        var scoreEntity = entity.getScoreInfo();
+        var scoreDto = dto.getScoreInfo();
+        assertTrue(
+                scoreEntity.getHomeGoals().equals(scoreDto.getHomeGoals()) &&
+                        scoreEntity.getAwayGoals().equals(scoreDto.getAwayGoals())
+        );
+
+        // penalties equal
+        var penaltiesEntity = entity.getPenaltiesInfo();
+        var penaltiesDto = dto.getPenaltiesInfo();
+        assertTrue(
+                penaltiesEntity.getHomePenalties().equals(penaltiesDto.getHomePenalties()) &&
+                        penaltiesEntity.getAwayPenalties().equals(penaltiesDto.getAwayPenalties())
         );
 
         // results equal
