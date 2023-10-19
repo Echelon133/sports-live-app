9fb35258c62935ee275ab4750c7b0b260a8de6ad
Mateusz <echelon133@gmail.com>
2023-10-19 17:32:34 +0200

    Move constraint validators into annotations that use them

--- match-service/src/main/java/ml/echelon133/matchservice/match/model/validator/ValidLocalDateTimeFormat.java
+++ match-service/src/main/java/ml/echelon133/matchservice/match/model/validator/ValidLocalDateTimeFormat.java
@@ -1,20 +1,50 @@
 package ml.echelon133.matchservice.match.model.validator;
 
-import ml.echelon133.matchservice.match.service.MatchService;
+import ml.echelon133.common.constants.DateFormatConstants;
 
 import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = LocalDateTimeValidator.class)
+@Constraint(validatedBy = ValidLocalDateTimeFormat.Validator.class)
 public @interface ValidLocalDateTimeFormat {
-    String dateFormat() default MatchService.DATE_OF_MATCH_FORMAT;
+    String dateFormat() default DateFormatConstants.DATE_TIME_FORMAT;
     String message() default "required date format {dateFormat}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+
+    class Validator implements ConstraintValidator<ValidLocalDateTimeFormat, String> {
+
+        private DateTimeFormatter dateTimeFormatter;
+
+        @Override
+        public void initialize(ValidLocalDateTimeFormat constraintAnnotation) {
+            this.dateTimeFormatter = DateTimeFormatter.ofPattern(constraintAnnotation.dateFormat());
+        }
+
+        @Override
+        public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+            if (s == null) {
+                // let @NotNull do its job
+                return true;
+            }
+
+            try {
+                LocalDateTime.parse(s, dateTimeFormatter);
+                return true;
+            } catch (DateTimeParseException ignore) {
+                return false;
+            }
+        }
+    }
 }
--- match-service/src/main/java/ml/echelon133/matchservice/player/model/validator/ValidLocalDateFormat.java
+++ match-service/src/main/java/ml/echelon133/matchservice/player/model/validator/ValidLocalDateFormat.java
@@ -1,21 +1,51 @@
 package ml.echelon133.matchservice.player.model.validator;
 
 
-import ml.echelon133.matchservice.player.service.PlayerService;
+import ml.echelon133.common.constants.DateFormatConstants;
 
 import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = LocalDateValidator.class)
+@Constraint(validatedBy = ValidLocalDateFormat.Validator.class)
 public @interface ValidLocalDateFormat {
-    String dateFormat() default PlayerService.DATE_OF_BIRTH_FORMAT;
+    String dateFormat() default DateFormatConstants.DATE_FORMAT;
     String message() default "required date format {dateFormat}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+
+    class Validator implements ConstraintValidator<ValidLocalDateFormat, String> {
+
+        private DateTimeFormatter dateTimeFormatter;
+
+        @Override
+        public void initialize(ValidLocalDateFormat constraintAnnotation) {
+            this.dateTimeFormatter = DateTimeFormatter.ofPattern(constraintAnnotation.dateFormat());
+        }
+
+        @Override
+        public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+            if (s == null) {
+                // let @NotNull do its job
+                return true;
+            }
+
+            try {
+                LocalDate.parse(s, dateTimeFormatter);
+                return true;
+            } catch (DateTimeParseException ignore) {
+                return false;
+            }
+        }
+    }
 }
