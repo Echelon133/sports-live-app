42b323a989b0c96532017a285b27c545489ac776
Mateusz <echelon133@gmail.com>
2023-10-26 18:53:09 +0200

    Implement `@PlayerExists` which uses `PlayerRepository` to check if a player exists before the `UpsertTeamPlayerDto` even gets to the service layer

---
+++ match-service/src/main/java/ml/echelon133/matchservice/player/constraints/PlayerExists.java
@@ -0,0 +1,47 @@
+package ml.echelon133.matchservice.player.constraints;
+
+import ml.echelon133.matchservice.player.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.UUID;
+
+@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PlayerExists.Validator.class)
+public @interface PlayerExists {
+    String message() default "id does not belong to a valid player";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    class Validator implements ConstraintValidator<PlayerExists, String> {
+
+        private PlayerRepository playerRepository;
+
+        public Validator() {}
+
+        @Autowired
+        public Validator(PlayerRepository playerRepository) {
+            this.playerRepository = playerRepository;
+        }
+
+        @Override
+        public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+            if (s == null) {
+                return true;
+            }
+            try {
+                return playerRepository.existsByIdAndDeletedFalse(UUID.fromString(s));
+            } catch (IllegalArgumentException ignore) {
+                return false;
+            }
+        }
+    }
+}
