67ec61eaf07b4527ff182688b3164317899decd0
Mateusz <echelon133@gmail.com>
2023-12-13 14:18:52 +0100

    Add custom MixIns to app's `ObjectMapper` for proper serialization/deserialization of `InsertMatchEvent` and `MatchEventDetails` objects

--- match-service/src/main/java/ml/echelon133/matchservice/MatchServiceApplication.java
+++ match-service/src/main/java/ml/echelon133/matchservice/MatchServiceApplication.java
@@ -1,8 +1,13 @@
 package ml.echelon133.matchservice;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import ml.echelon133.common.constants.DateFormatConstants;
+import ml.echelon133.common.event.MatchEventDetailsMixIn;
+import ml.echelon133.common.event.dto.MatchEventDetails;
+import ml.echelon133.matchservice.event.model.dto.InsertMatchEventMixIn;
+import ml.echelon133.matchservice.event.model.dto.InsertMatchEvent;
 import ml.echelon133.matchservice.match.controller.validators.MatchCriteriaValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,7 +26,19 @@ public class MatchServiceApplication {
 
 	@Bean
 	public static ObjectMapper objectMapper() {
-		return new ObjectMapper().registerModules(new JavaTimeModule());
+		var mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+		// add information about serialization/deserialization of InsertMatchEvent's subclasses
+		mapper.addMixIn(InsertMatchEvent.class, InsertMatchEventMixIn.class);
+
+		// add information about serialization/deserialization of MatchEventDetails' subclasses
+		mapper.addMixIn(MatchEventDetails.class, MatchEventDetailsMixIn.class);
+
+		// enable java time module
+		mapper.registerModules(new JavaTimeModule());
+
+		return mapper;
 	}
 
 	@Bean
