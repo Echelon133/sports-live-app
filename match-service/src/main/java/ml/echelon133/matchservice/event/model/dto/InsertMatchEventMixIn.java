e707193b0269f229a7652bfb0cc6811b7c0346f2
Mateusz <echelon133@gmail.com>
2023-12-14 11:23:40 +0100

    Fix duplicate serialization of `type` when serializing InsertMatchEvent subclasses

--- match-service/src/main/java/ml/echelon133/matchservice/event/model/dto/InsertMatchEventMixIn.java
+++ match-service/src/main/java/ml/echelon133/matchservice/event/model/dto/InsertMatchEventMixIn.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  */
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
         property = "type",
         visible = true
 )
