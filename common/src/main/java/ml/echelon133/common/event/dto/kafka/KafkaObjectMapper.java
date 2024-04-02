e52bae5016cc90c1601b40334e964f95fa3d9528
Mateusz <echelon133@gmail.com>
2024-04-02 21:45:18 +0200

    Add an ObjectMapper which can be used by Kafka to serialize/deserialize `MatchEventDetails`

---
+++ common/src/main/java/ml/echelon133/common/event/dto/kafka/KafkaObjectMapper.java
@@ -0,0 +1,26 @@
+package ml.echelon133.common.event.dto.kafka;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ml.echelon133.common.event.MatchEventDetailsMixIn;
+import ml.echelon133.common.event.dto.MatchEventDetails;
+
+/**
+ * {@link ObjectMapper} used by Kafka to serialize/deserialize {@link MatchEventDetails}.
+ */
+public class KafkaObjectMapper {
+
+    private static class Loader {
+        static final ObjectMapper INSTANCE;
+
+        static {
+            INSTANCE = new ObjectMapper();
+            INSTANCE.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            INSTANCE.addMixIn(MatchEventDetails.class, MatchEventDetailsMixIn.class);
+        }
+    }
+
+    public static ObjectMapper getInstance() {
+        return Loader.INSTANCE;
+    }
+}
