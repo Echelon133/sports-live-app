fa4955baab7d6c3338e31d0927761ff86a18d2d3
Mateusz <echelon133@gmail.com>
2024-12-03 19:22:03 +0100

    Remove the `FeignAutoConfiguration` fix that's no longer necessary after bumping the spring-cloud.version

--- competition-service/src/main/java/pl/echelon133/competitionservice/CompetitionServiceApplication.java
+++ competition-service/src/main/java/pl/echelon133/competitionservice/CompetitionServiceApplication.java
@@ -1,7 +1,6 @@
 package pl.echelon133.competitionservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,9 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
-// TODO: Remove this autoconfiguration import when spring-cloud.version gets bumped and this fix is not required anymore
-// Fixes https://stackoverflow.com/questions/74593433/consider-defining-a-bean-of-type-org-springframework-cloud-openfeign-feignconte
-@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableFeignClients
 public class CompetitionServiceApplication {
 
