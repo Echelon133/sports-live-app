64957d559467a4ec93a857ebc2867a6b068cf8be
Mateusz <echelon133@gmail.com>
2025-01-27 21:18:42 +0100

    Implement a way of fetching info about unassigned matches via the match-service
    The competition-service knows the ids of unassigned matches. The actual
    information about these matches (who plays against who, etc.) is
    accessible via the match-service's `GET /api/matches?matchIds` endpoint.

--- competition-service/src/main/java/pl/echelon133/competitionservice/competition/client/MatchServiceClient.java
+++ competition-service/src/main/java/pl/echelon133/competitionservice/competition/client/MatchServiceClient.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.echelon133.competitionservice.competition.model.TeamDetailsDto;
+import pl.echelon133.competitionservice.competition.model.UnassignedMatchDto;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,4 +16,7 @@ public interface MatchServiceClient {
 
     @GetMapping("/api/teams")
     Page<TeamDetailsDto> getTeamByTeamIds(@RequestParam List<UUID> teamIds, Pageable pageable);
+
+    @GetMapping("/api/matches")
+    List<UnassignedMatchDto> getMatchesById(@RequestParam List<UUID> matchIds);
 }
