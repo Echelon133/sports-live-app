216817e12e76918d8d0a5517618dd1b6a90dfb30
Mateusz <echelon133@gmail.com>
2025-01-27 21:15:34 +0100

    Make repository return unassigned matches in a `Page` instead of a List
    This enables the API client to specify how many unassigned matches they
    want to see at a time, since there could be hundreds of these at a time,
    and it does not make any sense to always fetch all of them.

--- competition-service/src/main/java/pl/echelon133/competitionservice/competition/repository/UnassignedMatchRepository.java
+++ competition-service/src/main/java/pl/echelon133/competitionservice/competition/repository/UnassignedMatchRepository.java
@@ -1,11 +1,12 @@
 package pl.echelon133.competitionservice.competition.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.echelon133.competitionservice.competition.model.UnassignedMatch;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface UnassignedMatchRepository extends JpaRepository<UnassignedMatch, UUID> {
-    List<UnassignedMatch> findAllByUnassignedMatchId_CompetitionIdAndAssignedFalse(UUID competitionId);
+    Page<UnassignedMatch> findAllByUnassignedMatchId_CompetitionIdAndAssignedFalse(UUID competitionId, Pageable pageable);
 }
