477bc96623c020500f878008f0a8ee58104eb46e
Mateusz <echelon133@gmail.com>
2025-03-16 21:04:26 +0100

    Explicitly configure deserialization of `Page` in competition-service
    Without this the feign client fetching `Page` objects from the
    match-service throws, since it does not "know" how to reconstruct a page
    object from the received JSON.

--- competition-service/src/main/java/pl/echelon133/competitionservice/CompetitionServiceApplication.java
+++ competition-service/src/main/java/pl/echelon133/competitionservice/CompetitionServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.PageJacksonModule;
+import org.springframework.cloud.openfeign.support.SortJacksonModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -29,6 +31,10 @@ public class CompetitionServiceApplication {
         var mapper = new ObjectMapper();
         // enable java time module
         mapper.registerModules(new JavaTimeModule());
+        // these have to be configured manually, otherwise deserializing Page objects received via the feign client
+        // fails and throws an exception
+        mapper.registerModules(new PageJacksonModule());
+        mapper.registerModule(new SortJacksonModule());
         return mapper;
     }
 
