0bee333887e3405f1e279cc62330e75b1f1f1345
Mateusz <echelon133@gmail.com>
2025-03-18 16:20:57 +0100

    Deserialize all `TeamDetailsDto` fields received via the feign client

--- competition-service/src/main/java/pl/echelon133/competitionservice/competition/model/TeamDetailsDto.java
+++ competition-service/src/main/java/pl/echelon133/competitionservice/competition/model/TeamDetailsDto.java
@@ -2,5 +2,16 @@ package pl.echelon133.competitionservice.competition.model;
 
 import java.util.UUID;
 
-public record TeamDetailsDto(UUID id, String name, String crestUrl) {
+public record TeamDetailsDto(
+        UUID id,
+        String name,
+        String crestUrl,
+        String countryCode,
+        CoachDto coach
+) {
+    public TeamDetailsDto(UUID id, String name, String crestUrl) {
+        this(id, name, crestUrl, null, null);
+    }
+
+    public record CoachDto(UUID id, String name) {}
 }
